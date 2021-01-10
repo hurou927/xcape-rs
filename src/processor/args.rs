d736987788c0fd749bcb54ce46f0103ef074e04d
hurou927 <god.be.with.ye.fs@gmail.com>
2021-01-10 21:23:25 +0900

    simple parse map

--- src/processor/args.rs
+++ src/processor/args.rs
@@ -1,7 +1,13 @@
-
 use clap::{App, Arg};
 
-use super::context::Context; 
+use super::context::Context;
+
+fn parse_map(map: &str) -> (u8, Vec<u8>) {
+    let v: Vec<&str> = map.split('=').collect();
+    let key = v[0].parse::<u8>().unwrap();
+    let vals: Vec<u8> = v[1].split('|').map(|m| m.parse::<u8>().unwrap()).collect();
+    (key, vals)
+}
 
 pub fn parse() -> Context {
     let app = App::new("xcape-rs")
@@ -9,26 +15,38 @@ pub fn parse() -> Context {
         .about("implement xcape with Rust")
         .arg(
             Arg::with_name("map")
-            .help("format: {keycode}={keycode}")
-            .takes_value(true)
-            .short("e")
-            .long("expression")
-            .multiple(true)
+                .help("format: {keycode}={keycode}")
+                .takes_value(true)
+                .short("e")
+                .long("expression")
+                .multiple(true),
         )
         .arg(
             Arg::with_name("debug")
-            .help("debug flag")
-            .short("d")
-            .long("debug")
-            )
+                .help("debug flag")
+                .short("d")
+                .long("debug"),
+        )
         .get_matches();
 
-    if let Some(in_v) = app.values_of("map") {
-        for in_file in in_v {
-            println!("map: {}", in_file);
+    if let Some(maps) = app.values_of("map") {
+        for map in maps {
+            let v: Vec<&str> = map.split('=').collect();
+            println!("map: {:?} {:?}", map, v);
         }
-    } 
-
+    }
 
     Context::new(app.is_present("debug"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_map_success() {
+        assert_eq!(parse_map("16=32"), (16, vec![32]));
+        assert_eq!(parse_map(" 16 = 32 "), (16, vec![32]));
+        assert_eq!(parse_map("16=32|53|21"), (16, vec![32, 53, 21]));
+    }
 }
