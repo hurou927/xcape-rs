3810bed3e83c19ec7ea1047158c49496cc7f1bec
hurou927 <god.be.with.ye.fs@gmail.com>
2021-01-10 19:58:22 +0900

    add simple arg parser

--- src/processor/args.rs
+++ src/processor/args.rs
@@ -1,5 +1,37 @@
 
+use clap::{App, Arg};
 
 pub fn parse() {
+    let app = App::new("xcape-rs")
+        .version("1.0")
+        .about("implement xcape with Rust")
+        .arg(
+            Arg::with_name("map")
+            .help("format: {keycode}={keycode}")
+            .takes_value(true)
+            .short("e")
+            .long("expression")
+            .multiple(true)
+        )
+        .arg(
+            Arg::with_name("debug")
+            .help("debug flag")
+            .short("d")
+            .long("debug")
+            )
+        .get_matches();
+
+    if let Some(in_v) = app.values_of("map") {
+        for in_file in in_v {
+            println!("map: {}", in_file);
+        }
+    } 
+
+    //println!("flg is {}", if app.is_present("debug") {"ON"} else {"OFF"});
+
+    if app.is_present("debug") {
+        // env::set_var("RUST_LOG", "debug");
+        println!("debug mode");
+    }
     println!("parse")
 }
